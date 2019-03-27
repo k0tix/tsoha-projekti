df570d19fcbeeab44bb964ecd4003aaa79e3f9d7
k0tix <otto.pirttimaa@gmail.com>
2019-03-27 11:42:26 +0200

    add a way to modify item price

--- application/items/views.py
+++ application/items/views.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.items.models import Item
+import json
 
 @app.route("/items/", methods=["GET"])
 def items_index():
@@ -9,6 +10,15 @@ def items_index():
 @app.route("/items/new/")
 def items_form():
     return render_template("items/new.html")
+
+@app.route("/items/<item_id>/", methods=["POST"])
+def items_set_price(item_id):
+
+    t = Item.query.get(item_id)
+    t.price = request.form.get("price")
+    db.session().commit()
+
+    return redirect(url_for("items_index"))
 
 @app.route("/items/", methods=["POST"])
 def items_create():
