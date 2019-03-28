f0b1a8d796290a19b83e0e80b48aabe6e143735b
k0tix <otto.pirttimaa@gmail.com>
2019-03-28 12:49:39 +0200

    add margin

--- application/static/js/components/PriceForm.jsx
+++ application/static/js/components/PriceForm.jsx
@@ -8,13 +8,15 @@ const PriceForm = ({id, originalPrice, updateItem}) => {
     const [price, setPrice] = useState(originalPrice)
 
     return (
-        <Form onSubmit={() => updateItem(id, price)}>
-            <Form.Field>
-                <label>Change price</label>
-                <input onChange={(event) => setPrice(event.target.value)} type='number' placeholder='New price' value={price}/>
-            </Form.Field>
-            <Button primary type='submit'>Submit</Button>
-        </Form>
+        <div style={{margin: '1em'}}>
+            <Form size='small' onSubmit={() => updateItem(id, price)}>
+                <Form.Field>
+                    <label>Change price</label>
+                    <input onChange={(event) => setPrice(event.target.value)} type='number' placeholder='New price' value={price}/>
+                </Form.Field>
+                <Button primary type='submit'>Submit</Button>
+            </Form>
+        </div>
     )
 }
 
