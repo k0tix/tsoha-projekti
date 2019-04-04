be4a9704157834b1c94a3c39c08a2ff5920f09d8
k0tix <otto.pirttimaa@gmail.com>
2019-04-04 21:29:31 +0300

    add import flash

--- application/items/views.py
+++ application/items/views.py
@@ -1,5 +1,5 @@
 from application import app, db
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for, flash
 from flask_login import login_required, current_user
 
 from application.items.models import Item
@@ -37,25 +37,34 @@ def items_create():
 
     return redirect(url_for("items_index"))
 
-@app.route("/items/<item_id>/", methods=["PUT"])
+@app.route("/items/<item_id>", methods=["POST"])
 @login_required
 def items_set_price(item_id):
     i = Item.query.get(item_id)
-    i.price = request.form.price.data
+    i.price = request.form["price"]
     db.session().commit()
     return redirect(url_for("items_index"))
 
-@app.route("/items/purchase/<item_id>")
+@app.route("/items/purchase/<item_id>", methods=["POST"])
 @login_required
 def items_purchase(item_id):
     i = Item.query.get(item_id)
+
+    if not i:
+        flash("Item not found")
+        return redirect(url_for("items_index"))
+
     user = current_user
 
     if(i.price > user.balance):
+        flash("Not enough balance")
         return redirect(url_for("items_index"))
 
-    p = Purchase(current_user.id, item_id)
+    p = Purchase(current_user.id, i.id)
     db.session.add(p)
-    user.balance -= item.price
+    i.sold = True
+    user.balance -= i.price
     db.session().commit()
+    flash("Purchase successfull")
+    return redirect(url_for("items_index"))
     
