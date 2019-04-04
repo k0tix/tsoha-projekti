f1e8a532512608c44d292de1d98e19e36d86128c
k0tix <otto.pirttimaa@gmail.com>
2019-04-04 20:32:42 +0300

    let flask handle routes

--- application/views.py
+++ application/views.py
@@ -2,6 +2,5 @@ from application import app
 from flask import render_template
 
 @app.route("/", defaults={"path": ''})
-@app.route("/<path:path>")
 def index(path):
     return render_template("index.html")
