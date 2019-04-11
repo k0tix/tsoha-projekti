ef20ea9a3ff6b0c264a8d8149b6d5ca8a32d82cd
k0tix <otto.pirttimaa@gmail.com>
2019-04-11 19:51:15 +0300

    add form validations

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,19 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, Email
 
 class LoginForm(FlaskForm):
-    username = StringField("username")
-    password = PasswordField("password")
+    username = StringField("username", validators=[InputRequired("Please enter your username.")])
+    password = PasswordField("password", validators=[InputRequired("Please enter your password.")])
 
     class Meta:
         csrf = False
 
 class RegisterForm(FlaskForm):
-    name = StringField("name")
-    username = StringField("username")
-    email = StringField("email")
-    password = PasswordField("password")
-    role = StringField("role")
+    name = StringField("name", validators=[InputRequired("Please enter your name.")])
+    username = StringField("username", validators=[InputRequired("Please enter your username.")])
+    email = EmailField("email", validators=[InputRequired("Please enter your email address."), Email("This field requires a valid email address")])
+    password = PasswordField("password", validators=[InputRequired("Please enter your password.")])
+    role = StringField("role", validators=[InputRequired("Please enter a role")])
 
     class Meta:
         csrf = False
