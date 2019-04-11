73aba81ee0e2c9109a09719dc1b1c417ba8bd6c3
k0tix <otto.pirttimaa@gmail.com>
2019-04-11 15:49:23 +0300

    add login_view to init file

--- application/__init__.py
+++ application/__init__.py
@@ -43,6 +43,7 @@ from flask_login import LoginManager
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
 @login_manager.user_loader
