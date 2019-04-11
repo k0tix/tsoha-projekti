eb26e636c32d1c264028f822d367c75e102969f8
k0tix <otto.pirttimaa@gmail.com>
2019-04-11 21:23:08 +0300

    add different views for user accounts

--- application/user/views.py
+++ application/user/views.py
@@ -1,5 +1,5 @@
 
-from flask import redirect, render_template, request, url_for
+from flask import redirect, render_template, request, url_for, flash
 from flask_login import current_user
 
 from application import app, db, login_required
@@ -7,8 +7,13 @@ from application.auth.models import User
 
 @app.route("/user/<user_username>", methods=["GET"])
 def user_index(user_username):
+    user = User.query.filter_by(username=user_username).first()
+
+    if not user:
+        flash("No user was found")
+        return redirect(url_for("items_index"))
+
     if current_user.is_anonymous == True:
-        user = User.query.filter_by(username=user_username).first()
         return render_template("user/preview.html", user=user)
     elif current_user.username == user_username:
         purchases = User.find_users_purchases(current_user.id)
@@ -18,7 +23,6 @@ def user_index(user_username):
         average_quality = User.get_average_quality_purchased(current_user.id)
         return render_template("user/user.html", user=current_user, purchases=purchases, total_money=total, average_quality=average_quality)
     else:
-        user = User.query.filter_by(username=user_username).first()
         return render_template("user/preview.html", user=user)
 
 @app.route("/user/ban/<user_id>", methods=["POST"])
