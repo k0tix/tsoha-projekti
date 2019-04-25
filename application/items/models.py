abb53a7c0556264bf37901a7cbb21e8628e9e96c
k0tix <otto.pirttimaa@gmail.com>
2019-04-25 12:47:27 +0300

    Rename item_float to quality and add a query that joins username to item

--- application/items/models.py
+++ application/items/models.py
@@ -1,5 +1,7 @@
 from application import db
 
+from sqlalchemy.sql import text
+
 ### MODEL ###
 
 class Item(db.Model):
@@ -10,16 +12,28 @@ class Item(db.Model):
 
     name = db.Column(db.String(144), nullable=False)
     price = db.Column(db.Integer, nullable=False)
-    item_float = db.Column(db.Float, nullable=False) #Describes how good the item is from 0-1. 1 is perfect quality and 0 is worst quality
+    quality = db.Column(db.Float, nullable=False) #Describes how good the item is from 0-1. 1 is perfect quality and 0 is worst quality
     item_type = db.Column(db.String(144), nullable=False)
     sold = db.Column(db.Boolean, nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'),
                            nullable=False)
 
-    def __init__(self, name, price, item_float, item_type):
+    def __init__(self, name, price, quality, item_type):
         self.name = name
         self.price = price
-        self.item_float = item_float
+        self.quality = quality
         self.item_type = item_type
         self.sold = False
+
+    @staticmethod
+    def find_items_with_username():
+        stmt = text("SELECT Item.id, Item.name, Item.price, Item.quality, Item.item_type, Item.sold, Account.username FROM Item"
+                    " JOIN Account ON Account.id = Item.account_id"
+                    " WHERE Item.sold = FALSE")
+
+        res = db.engine.execute(stmt)
+
+        response = [{"id": row[0], "name": row[1], "price": row[2], "quality": row[3], "item_type": row[4], "sold": row[5], "seller_username": row[6]} for row in res]
+
+        return response
