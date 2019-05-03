f405f5716c2a7c3068dcb52116d128de17e66c60
k0tix <otto.pirttimaa@gmail.com>
2019-05-03 21:17:52 +0300

    update model

--- application/items/models.py
+++ application/items/models.py
@@ -13,7 +13,7 @@ class Item(db.Model):
     name = db.Column(db.String(144), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     quality = db.Column(db.Float, nullable=False) # Describes how good the item is from 0-1. 1 is perfect quality and 0 is worst quality
-    item_type = db.Column(db.String(144), nullable=False, index=True)
+    item_type = db.Column(db.String(144), nullable=False)
     sold = db.Column(db.Boolean, nullable=False)
 
     account_id = db.Column(db.Integer, db.ForeignKey("account.id"),
